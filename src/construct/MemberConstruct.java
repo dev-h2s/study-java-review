e21a00236401f1083185bc77c01126eec33e2fe3
gugigugi <2esc2esch@gmail.com>
2024-02-14 01:44:05 +0900

    생성자:   생성자는 클래스와 같은이름     장점: 직접 생성한 생성자를 반드시 호출한다     필수로 입력하기때문에 아무 정보가없는 null이 들어가는 유령회원이 등장할 가능성을 원천 차단

--- src/construct/MemberConstruct.java
+++ src/construct/MemberConstruct.java
@@ -9,6 +9,7 @@ public class MemberConstruct {
     // 장점: 직접 생성한 생성자를 반드시 호출한다
     // 필수로 입력하기때문에 아무 정보가없는 null이 들어가는 유령회원이 등장할 가능성을 원천 차단
 
+
     MemberConstruct(String name, int age, int grade){
         System.out.println("생성자 호출 name = "+name+"age="+age+"grade="+grade);
         this.name = name;
