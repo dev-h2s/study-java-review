0cdb3b65b8f22b45061a1891cfbbc720581cad9c
gugigugi <2esc2esch@gmail.com>
2024-02-15 01:44:17 +0900

    패키지 분류 사용

--- src/pack/PackageMain1.java
+++ src/pack/PackageMain1.java
@@ -2,6 +2,7 @@ package pack;
 
 public class PackageMain1 {
     public static void main(String[] args) {
+
         Data data = new Data();
         // 다른 패키지 불러오는법
         pack.a.User user = new pack.a.User();
