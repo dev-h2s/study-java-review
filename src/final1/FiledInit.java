be5bb2294803aede3608eaf802b2e3969674e7d1
dev-h2s <dev.2esc2esch@gmail.com>
2024-03-24 18:21:24 +0900

    fianal final과 필드초기화시는 static final 사용 스태틱 final을 쓰면 메모리에 같은값만 있는 비효율적인 final 인스턴스를 대체가능

---
+++ src/final1/FiledInit.java
@@ -0,0 +1,10 @@
+package final1;
+
+public class FiledInit {
+    static final int CONST_VALUE =10;
+    final int value = 10; // 초기값이 final이면 생성자에서도 못바꿈
+
+//    public FiledInit(int value){
+////        this.value = value;
+//    }
+}
