c967cae3e4f02b03318cd6a243bce63114a2e6cf
dev-h2s <dev.2esc2esch@gmail.com>
2024-03-24 19:27:22 +0900

    공통되는 부분 상속으로 처리하는것이 좋다 다중 상속 지원 x 인터페이스는 다중 구현 가능

---
+++ src/extend1/ex2/CarMain.java
@@ -0,0 +1,15 @@
+package extend1.ex2;
+
+public class CarMain {
+    public static void main(String[] args) {
+        ElectronicCar electronicCar = new ElectronicCar();
+        electronicCar.move();
+        electronicCar.charge();
+
+        GasCar gasCar = new GasCar();
+        gasCar.fillUp();
+        gasCar.move();
+
+    }
+
+}
