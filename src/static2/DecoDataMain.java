b1e9160081ac92bb11451d4bf9742fe341305136
dev-h2s <dev.2esc2esch@gmail.com>
2024-03-24 16:30:13 +0900

    스태틱 메서드 import 시 클래스 명 생략가능 클래스 메서드도 인스터스를 통한 호출 가능 > 추천안함(클래스 메서드인지 헷갈리기떄문

--- src/static2/DecoDataMain.java
+++ src/static2/DecoDataMain.java
@@ -1,9 +1,16 @@
 package static2;
 
+import static static2.DecoData.*; // 클래스명 생략가능
+
 public class DecoDataMain {
     public static void main(String[] args) {
-        System.out.println("정적호출");
+
         DecoData.staticCall();
+        staticCall();
+        staticCall();
+
+        System.out.println("정적호출");
+        staticCall();
         System.out.println();
 
         System.out.println("인스턴스 호출");
@@ -16,6 +23,14 @@ public class DecoDataMain {
         data2.instanceCall();
         System.out.println();
 
-        DecoData.staticCall(data1);
+        staticCall(data1);
+
+        //추가
+        //인스턴스를 통한 스태틱 메서드 접근 : 추천 안함
+        DecoData data3 = new DecoData();
+        data3.staticCall();
+
+        //클래스를 통한 접근
+        staticCall();
     }
 }
