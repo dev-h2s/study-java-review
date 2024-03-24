58e31b702bf47edf944180dfc8fca7ef1855c9ab
dev-h2s <dev.2esc2esch@gmail.com>
2024-03-24 18:51:46 +0900

    final인스턴스는 인스턴스 변수값은 변경 가능하다

---
+++ src/final1/FinalRefMain.java
@@ -0,0 +1,15 @@
+package final1;
+
+public class FinalRefMain {
+    public static void main(String[] args) {
+        final Data data = new Data();//참조형
+
+        //참조 대상의 값은 변경 가능
+        data.value = 10;
+        System.out.println(data.value);
+        data.value = 20;
+        System.out.println(data.value);
+        //인스턴스는 값을 바꿀수 있다 Data.value는 불가.
+
+    }
+}
