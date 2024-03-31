6a2f2de832f842cddc2fe183cd0ca72f7eb6e98b
dev-h2s <dev.2esc2esch@gmail.com>
2024-03-31 15:56:32 +0900

    업캐스팅은 캐스팅 생략 가능 다운캐스팅은 불가능

---
+++ src/poly/basic/CastingMain3.java
@@ -0,0 +1,13 @@
+package poly.basic;
+
+//upcating Vs downcasting
+public class CastingMain3 {
+    public static void main(String[] args) {
+    Child child = new Child();
+    Parent parent1 = child; // 업캐스팅은 (parent) 생략 가능
+    Parent parent2 = (Parent) child; // 업캐스팅은 (parent) 생략 가능
+
+        parent1.prentMethod();
+        parent2.prentMethod();
+    }
+}
